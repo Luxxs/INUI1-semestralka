46752d295836cb37ff61769a774e3ce8bd430389
Luxxs <lukasavasaki@seznam.cz>
2015-11-28 13:13:57 +0100

    GUI - rozpracované

---
+++ INUI1/INUI1/Converters/CellBackgroundBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿using INUI1.Model;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace INUI1.Converters
+{
+    class CellBackgroundBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Cell && (value as Cell).InPath)
+            {
+                return new SolidColorBrush(Colors.LightGray);
+            }
+            return new SolidColorBrush(Colors.White);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ INUI1/INUI1/Converters/CellNumberContentConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace INUI1.Converters
+{
+    class CellNumberContentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int && (int) value == 0)
+            {
+                return "";
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
